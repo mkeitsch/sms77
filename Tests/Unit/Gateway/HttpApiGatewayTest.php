3bf4e7e385db4aa98cc622f7db91a5d2a59c7d74
Markus <mail@markuskeitsch.com>
2016-10-03 14:29:19 +0200

    First test added

---
+++ Tests/Unit/Gateway/HttpApiGatewayTest.php
@@ -0,0 +1,86 @@
+<?php
+namespace MKcom\SMS77\Tests\Unit\Gateway;
+
+/*
+ * This file is part of the MKcom.SMS77 package.
+ */
+
+use PHPUnit_Framework_TestCase;
+
+class HttpApiGatewayTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function emptyTest()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function aSentSmsIsNotSentASecondTime()
+    {
+
+    }
+
+    public function ifNoConfigurationHasBeenSetDefaultsAreUsed()
+    {
+
+    }
+
+    public function ifSmsValuesHasNotBeenSetConfigurationIsUsed()
+    {
+
+    }
+
+    public function ifSmsValuesHasBeenSetTheseAreUsed()
+    {
+
+    }
+
+    public function postParametersAreSetIfNotDefault()
+    {
+
+    }
+
+    public function postParametersAreNotSetIfDefault()
+    {
+
+    }
+
+    public function exceptionWillBeThrownIfGatewayStatusNot100OnSend()
+    {
+
+    }
+
+    public function lastStatusWillBeUpdated()
+    {
+
+    }
+
+    public function messageIdMessageStatusAndGatewayStatusWillBeUpdated()
+    {
+
+    }
+
+    public function returnedCreditStatusIsParsedCorrectlyAsFloat()
+    {
+
+    }
+
+    public function returnedCreditStatusAsErrorCodeThrowsException()
+    {
+
+    }
+
+    public function exceptionWillBeThrownIfGatewayStatusNot100OnGetMessageStatus()
+    {
+
+    }
+
+    public function messageStatusWillBeUpdated()
+    {
+
+    }
+
+}
